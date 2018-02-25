c26324ece4d52d142dceeef85d6519ed583616e7
Nazariy <nazarglova@gmail.com>
2018-02-25 18:29:34 +0200

    async email validation

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -4,6 +4,7 @@ import {Router} from '@angular/router';
 
 import {UsersService} from '../../shared/services/users.service';
 import {User} from '../../shared/models/user.model';
+import {reject} from 'q';
 
 @Component({
   selector: 'wfm-registration',
@@ -19,7 +20,7 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmail.bind(this)),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
       'name': new FormControl(null, [Validators.required]),
       'agree': new FormControl(false, [Validators.requiredTrue]),
@@ -38,6 +39,19 @@ export class RegistrationComponent implements OnInit {
           }
         });
       });
+  }
+
+  forbiddenEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserByEmail(control.value)
+        .subscribe((user: User) => {
+          if (user) {
+            resolve({forbiddenEmail: true});
+          } else {
+            return resolve(null);
+          }
+        });
+    });
   }
 
 }
--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -2,6 +2,6 @@ export class User {
   constructor(public email: string,
               public password: string,
               public name: string,
-              public id?: number,) {
+              public id?: number) {
   }
 }
